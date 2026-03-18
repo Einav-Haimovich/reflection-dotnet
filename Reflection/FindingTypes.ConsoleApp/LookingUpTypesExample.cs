a2a4d3d4162137fda4a627dedf87890b04f1169b
Einav Haimovich <einav@haimol.net>
2026-03-18 13:04:46 +0200

    Member Information

--- Reflection/FindingTypes.ConsoleApp/LookingUpTypesExample.cs
+++ Reflection/FindingTypes.ConsoleApp/LookingUpTypesExample.cs
@@ -1,70 +1,70 @@
-﻿//using System.Reflection;
+﻿using System.Reflection;
 
-//public sealed class LookingUpTypesExample
-//{
-//    public void Run()
-//    {
-//        Console.WriteLine("Type by typeof:");
-//        var typeofA = typeof(MyTypeA);
-//        var typeofB = typeof(MyTypeB);
-//        Console.WriteLine(typeofA.AssemblyQualifiedName);
-//        Console.WriteLine(typeofB.AssemblyQualifiedName);
-//        Console.WriteLine();
+public sealed class LookingUpTypesExample
+{
+    public void Run()
+    {
+        Console.WriteLine("Type by typeof:");
+        var typeofA = typeof(MyTypeA);
+        var typeofB = typeof(MyTypeB);
+        Console.WriteLine(typeofA.AssemblyQualifiedName);
+        Console.WriteLine(typeofB.AssemblyQualifiedName);
+        Console.WriteLine();
 
-//        Console.WriteLine("Type by name:");
-//        var typeCByName = Type.GetType("MyTypeC");
-//        var typeDByName = Type.GetType("MyTypeC+MyTypeD");
-//        var typeEByName = Type.GetType("MyTypeC+MyTypeE");
-//        Console.WriteLine(typeCByName.AssemblyQualifiedName);
-//        Console.WriteLine(typeDByName.AssemblyQualifiedName);
-//        Console.WriteLine(typeEByName.AssemblyQualifiedName);
-//        Console.WriteLine();
+        Console.WriteLine("Type by name:");
+        var typeCByName = Type.GetType("MyTypeC");
+        var typeDByName = Type.GetType("MyTypeC+MyTypeD");
+        var typeEByName = Type.GetType("MyTypeC+MyTypeE");
+        Console.WriteLine(typeCByName.AssemblyQualifiedName);
+        Console.WriteLine(typeDByName.AssemblyQualifiedName);
+        Console.WriteLine(typeEByName.AssemblyQualifiedName);
+        Console.WriteLine();
 
-//        Console.WriteLine("Type by name vs namespace:");
-//        var typeAByName = Type.GetType("MyTypeA");
-//        var typeAFromCoolNamespace = Type.GetType("CoolNamespace.MyTypeA");
-//        Console.WriteLine(typeAByName.AssemblyQualifiedName);
-//        Console.WriteLine(typeAFromCoolNamespace.AssemblyQualifiedName);
-//        Console.WriteLine();
+        Console.WriteLine("Type by name vs namespace:");
+        var typeAByName = Type.GetType("MyTypeA");
+        var typeAFromCoolNamespace = Type.GetType("CoolNamespace.MyTypeA");
+        Console.WriteLine(typeAByName.AssemblyQualifiedName);
+        Console.WriteLine(typeAFromCoolNamespace.AssemblyQualifiedName);
+        Console.WriteLine();
 
-//        Console.WriteLine("Type from assembly:");
-//        var typeAFromAssembly = Assembly.GetExecutingAssembly().GetType("MyTypeA");
-//        Console.WriteLine(typeAFromAssembly.AssemblyQualifiedName);
-//        Console.WriteLine();
+        Console.WriteLine("Type from assembly:");
+        var typeAFromAssembly = Assembly.GetExecutingAssembly().GetType("MyTypeA");
+        Console.WriteLine(typeAFromAssembly.AssemblyQualifiedName);
+        Console.WriteLine();
 
-//        Console.WriteLine("All types from assembly:");
-//        var allTypesFromAssembly = Assembly.GetExecutingAssembly().GetTypes();
-//        foreach (var type in allTypesFromAssembly)
-//        {
-//            Console.WriteLine(type.FullName);
-//        }
+        Console.WriteLine("All types from assembly:");
+        var allTypesFromAssembly = Assembly.GetExecutingAssembly().GetTypes();
+        foreach (var type in allTypesFromAssembly)
+        {
+            Console.WriteLine(type.FullName);
+        }
 
-//        Console.ReadLine();
-//    }
-//}
+        Console.ReadLine();
+    }
+}
 
-//public sealed class MyTypeA
-//{
-//}
+public sealed class MyTypeA
+{
+}
 
-//internal sealed class MyTypeB
-//{
-//}
+internal sealed class MyTypeB
+{
+}
 
-//public sealed class MyTypeC
-//{
-//    public sealed class MyTypeD
-//    {
-//    }
+public sealed class MyTypeC
+{
+    public sealed class MyTypeD
+    {
+    }
 
-//    private sealed class MyTypeE
-//    {
-//    }
-//}
+    private sealed class MyTypeE
+    {
+    }
+}
 
-//namespace CoolNamespace
-//{
-//    public sealed class MyTypeA
-//    {
-//    }
-//}
+namespace CoolNamespace
+{
+    public sealed class MyTypeA
+    {
+    }
+}
--- Reflection/FindingTypes.ConsoleApp/Program.cs
+++ Reflection/FindingTypes.ConsoleApp/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Run an example!");
+﻿//Console.WriteLine("Finding Types Example Application");
+//FindingTypesExampleApplication.Run();
+
+//Console.WriteLine("Finding Types Example Application with Filtering");
+//var filteringTypeInformation = new FilteringTypeInformationExample();
+//filteringTypeInformation.Run();
+
+Console.WriteLine("Looking Up Types Example Application");
+var lookingUpTypesExample = new LookingUpTypesExample();
+lookingUpTypesExample.Run();
+
+//Console.WriteLine("Assembly Scanning Basics Example Application");
+//var assemblyScanningBasicsExample = new AssemblyScanningBasicsExample();
+//assemblyScanningBasicsExample.Run();
